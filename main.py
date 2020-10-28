0af995646dc6b4e07f23883bc69c458d61e6aeb7
Peter Eichinger <peter.eichinger@gmail.com>
2020-10-28 08:27:08 +0100

    Add check for valid sudoku values

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from puzzle import Puzzle, block_generator
+from puzzle import Puzzle, row_generator, col_generator
 
 if __name__ == "__main__":
     test_puzzle = Puzzle("1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9")
     # test_puzzle.set((1, 1), 9)
 
-    for pos in block_generator((3, 1)):
-        print(pos)
-    print(test_puzzle)
+    print(test_puzzle.check())
--- puzzle/__init__.py
+++ puzzle/__init__.py
@@ -1,29 +1,37 @@
-from typing import Dict, Optional, Union, Tuple
+from typing import Dict, Optional, Union, Tuple,Iterable
 from . import PuzzleErrors
 from . import Types
+from itertools import chain
 
 PuzzleDict = Dict[Types.Position, Optional[int]]
 
 
+def _check_range(min_val, max_val, x: Union[int, Tuple[int, int]]):
+    if isinstance(x, int):
+        return min_val <= x <= max_val
+    else:
+        return _check_range(min_val, max_val, x[0]) and _check_range(min_val, max_val, x[1])
+
+
 def row_generator(row: int):
-    if row not in range(1, 10):
+    if not _check_range(1, 9, row):
         raise ValueError(row)
     for col in range(1, 10):
         yield col, row
 
 
 def col_generator(col: int):
-    if col not in range(1, 10):
+    if not _check_range(1, 9, col):
         raise ValueError(col)
     for row in range(1, 10):
         yield col, row
 
 
 def block_generator(block: Types.Block):
-    bx, by = block
-    if bx not in range(1, 4) or by not in range(1, 4):
-        raise ValueError((bx, by))
+    if not _check_range(1, 3, block):
+        raise ValueError(block)
 
+    bx, by = block
     for y in range(1, 4):
         for x in range(1, 4):
             yield (bx - 1) * 3 + x, (by - 1) * 3 + y
@@ -33,40 +41,50 @@ class Puzzle:
     def __init__(self, values: Union[str, PuzzleDict]):
 
         if isinstance(values, str):
-            self.values = self._parse(values)
+            self.values = self.__parse(values)
         else:
             self.values = values
-        self._validate()
+        self.__validate()
 
     def set(self, position: Types.Position, value: int):
-        self._validate_entry((position, value))
+        self.__validate_entry((position, value))
         self.values[position] = value
 
-    # def check(self) -> bool:
+    def check(self) -> bool:
+        return all(chain(
+            (self.__check_pos_range(col_generator(col)) for col in range(1, 10)),
+            (self.__check_pos_range(row_generator(row)) for row in range(1, 10)),
+            (self.__check_pos_range(block_generator((x, y))) for x in range(1, 4) for y in range(1, 4))
+        ))
+
+    def __check_pos_range(self, pos_range: Iterable):
+        items = [self.values[pos] for pos in pos_range if pos in self.values]
+        value_set = set(items)
+        return len(items) == len(value_set)
 
     @staticmethod
-    def _parse(text: str):
+    def __parse(text: str) -> PuzzleDict:
         index = 0
         values = {}
         for val in text.split(','):
             if val.strip():
-                values[(index // 9 + 1, index % 9 + 1)] = int(val)
+                values[(index % 9 + 1, index // 9 + 1)] = int(val)
             index += 1
         return values
 
-    def _validate(self):
+    def __validate(self):
         for entry in self.values.items():
-            self._validate_entry(entry)
+            self.__validate_entry(entry)
 
     @staticmethod
-    def _validate_entry(entry: Tuple[Tuple[int, int], Optional[int]]):
+    def __validate_entry(entry: Tuple[Tuple[int, int], Optional[int]]):
         (x, y), val = entry
-        if x not in range(1, 10) or y not in range(1, 10):
+        if not _check_range(1, 9, (x, y)):
             raise PuzzleErrors.PositionError((x, y))
-        if val not in range(1, 10):
+        if not _check_range(1, 9, val):
             raise PuzzleErrors.InvalidValueError((x, y), val)
 
     def __str__(self):
-        str_list = [str(self.values[(x, y)]) if (x, y) in self.values else "_" for x in range(1, 10) for y in
-                    range(1, 10)]
+        str_list = [str(self.values[(x, y)]) if (x, y) in self.values else "_" for y in
+                    range(1, 10) for x in range(1, 10)]
         return '\n'.join([''.join(str_list[i * 9:(i + 1) * 9]) for i in range(0, 10)])
